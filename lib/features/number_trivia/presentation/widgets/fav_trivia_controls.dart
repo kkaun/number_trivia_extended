5f0ef6f009c71e402090967fa55951be436b9f85
Kirill <nickulodeon@gmail.com>
2020-09-03 16:02:25 +0300

    Add share fav trivia feature

--- lib/features/number_trivia/presentation/widgets/fav_trivia_controls.dart
+++ lib/features/number_trivia/presentation/widgets/fav_trivia_controls.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:numbers_trivia/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:numbers_trivia/features/number_trivia/domain/usecases/insert_fav_trivia.dart';
 import 'package:numbers_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
-import '../../../../injection_container.dart';
+import 'package:share/share.dart';
 
 class FavoriteTriviaControls extends StatelessWidget {
   final NumberTrivia trivia;
@@ -18,7 +17,11 @@ class FavoriteTriviaControls extends StatelessWidget {
             alignment: Alignment.center,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: FloatingActionButton(child: Icon(Icons.share), onPressed: () {}),
+              child: FloatingActionButton(
+                  child: Icon(Icons.share),
+                  onPressed: () {
+                    _shareTriviaInfo(trivia);
+                  }),
             ),
           ),
         ),
@@ -43,5 +46,13 @@ class FavoriteTriviaControls extends StatelessWidget {
     // ignore: close_sinks
     final bloc = BlocProvider.of<NumberTriviaBloc>(context);
     bloc.add(InsertFavoriteTriviaEvent(trivia));
+  }
+
+  void _shareTriviaInfo(NumberTrivia trivia) {
+    StringBuffer sb = StringBuffer();
+    sb.writeAll(['Check out an interesting trivia:\n', 'Number: ${trivia.number}\n', 'Description: ${trivia.text}']);
+    final strToShare = sb.toString();
+    Share.share(strToShare);
+    sb.clear();
   }
 }
